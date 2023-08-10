5dae20159542cea1888049619a3af2bdf13e01bb
jennaccarlson <33760137+jennaccarlson@users.noreply.github.com>
2023-08-10 09:56:03 -0400

    Update 04_impute_example.sh

--- 04_impute_example.sh
+++ 04_impute_example.sh
@@ -1,14 +1,17 @@
-# begin with indexed vcf file of phased reference panel haplotypes (ref_panel_chr21.vcf.gz)
+# INPUT: an indexed vcf file of phased reference panel haplotypes (ref_panel_phased_chr21.vcf.gz)
+#        and an indexed vcf file of phased genotypes (genotypes_phased_chr21.vcf.gz)
+# OUTPUT: imputed genotypes and dosages (imputed_chr21.dose.vcf.gz) with corresponding info file (imputed_chr21.info)
+
 
 # create m3vcf file 
-Minimac3-omp --refHaps ref_panel_chr21.vcf.gz \ 
+Minimac3-omp --refHaps ref_panel_phased_chr21.vcf.gz \ 
              --processReference \ 
              --cpus 10 \ 
              --prefix ref_panel_chr21 
 
 # perform imputation using phased genotypes for target samples 
-minimac4 --refHaps ref_panel_chr21.m3vcf.gz \ 
-         --haps target_genotypes_phased_chr21.vcf.gz \ 
+minimac4 --refHaps ref_panel_phased_chr21.m3vcf.gz \ 
+         --haps genotypes_phased_chr21.vcf.gz \ 
          --prefix imputed_chr21 \ 
          --format GT,DS \ 
          --allTypedSites \ 
