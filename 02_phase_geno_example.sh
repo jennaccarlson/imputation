d74a6eac06f84a1b5f1c5c4bdc4b40f842ccf598
jennaccarlson <33760137+jennaccarlson@users.noreply.github.com>
2023-08-10 10:14:41 -0400

    Create 02_phase_geno_example.sh

---
+++ 02_phase_geno_example.sh
@@ -0,0 +1,9 @@
+# INPUT: aligned genotyped data from 01_align_example.sh (genotypes_aligned_chr21.vcf.gz)
+#        (Note: requires the genetic_map_hg38_withX.txt.gz file downloaded with Eagle)
+# OUTPUT: phased genotype data (genotypes_phased_chr21.vcf.gz)
+
+eagle --vcf=genotypes_aligned_chr21.vcf.gz \
+	  --geneticMapFile=genetic_map_hg38_withX.txt.gz \
+	  --chrom=21 \
+	  --outPrefix=genotypes_phased_chr21 \
+	  --numThreads=5
